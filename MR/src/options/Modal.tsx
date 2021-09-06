a4433a3d57c2ceb114ea01e6bf3dada53a686c06
Jungsoo Kim <sunpl13@naver.com>
2021-09-06 17:59:01 +0900

    modal UI 수정

--- MR/src/options/Modal.tsx
+++ MR/src/options/Modal.tsx
@@ -17,7 +17,7 @@ function Modal(props : IProps) {
          // 모달이 열릴때 openModal 클래스가 생성된다.
          <div className={ open ? 'openModal modal' : 'modal' }>
          { open ? (  
-             <section>
+             <section className = "modal_section">
                  <header>
                      {header}
                      <button className="close" onClick={() => close()}> &times; </button>
